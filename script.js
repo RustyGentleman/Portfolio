7b48ea93c6498fedf9942bf6180c4d36e4cddc31
Rasutei <rasutei@outlook.com>
2022-08-14 18:26:31 -0300

    Fully switched post-startup funcs to non-jQuery Switched to using arrays to manage transforms

--- script.js
+++ script.js
@@ -1,47 +1,57 @@
-var offsets = []
 $(document).ready(function(){
+	// ? Constants
 	const DEBUG = 1
 	const PERSPECTIVE = 1500
-	let mouse = {x: 0, y: 0, moved: false}
-	let $allpanels = $('.face')
-	let $mainpanels = $('#window-start .face')
-	let $windows = $('.window')
 	const isMobile = () => window.matchMedia('(hover: none) and (pointer: coarse)').matches
+	// ? Statekeepers
+	let offsets = []
+	let mouse = {x: 0, y: 0, moved: false}
+	// ? Quick-access collections
+	let $windows = Array.from(document.querySelectorAll('.window'))
+	let $current_window_id = document.querySelector('.window.current').id
+	let $panels = []
+	let $panels_start = []
+	// ? Performance guards
+	let interval = 16
+	let face_calls = 0
+	let allow_face_call = true
+	let interval_fps_setter
+	let interval_allowcall
 
-	//! Setup
-	//* Duplicate profile element,
-	//* add hover listener to panels
+	ResetIntervals()
+
+	// ! Setup
+	// * Duplicate profile element
+	// * Populate $panels and $panels_start collections
 	$('.window').each((_, w) => {
 		if (!$(w).children().filter('#profile')[0])
 			$(w).children().first().before($('#profile').clone(true))
-		// $(w).children().each((pi, pe) => {
-		// 	$(pe).on('mouseenter', function() {
-		// 		$('.window').each((ci, ce) => $($(ce).children()[pi]?.firstElementChild).addClass('hovered'))
-		// 	})
-		// 	.on('mouseleave', function() {
-		// 		$('.window').each((ci, ce) => $($(ce).children()[pi]?.firstElementChild).removeClass('hovered'))
-		// 	})
-		// })
-		$allpanels = $('#window-start .face')
+		if (w.id == 'window-start')
+			$panels_start = Array.from(document.getElementById('window-start').getElementsByClassName('face'))
+		Array.from(w.getElementsByClassName('face')).forEach((e, i) =>
+			$panels.push([e, i, w.id]))
 	})
-	//* Flash on click CSS thing
-	$allpanels.each((i, e) => {
-		$(e).on('click', () => setTimeout(() => document.activeElement.blur(), 100))
-		// $(e).css('--bg-anim-delay', Math.floor(Math.random()*10)+'ms')
-	})
-	//* Create shadow and chromatic aberration duplicates
+	// * Flash on click CSS thing
+	$panels.forEach((e, _) => 
+		$(e[0]).on('click', () => 
+			setTimeout(() => 
+				document.activeElement.blur(), 100)))
+	// * Create shadow and chromatic aberration duplicates
 	$('.panel-lining').each((_, e) => {
 		$(e).before($(e).clone().addClass('shadow'))
 		$(e).before($(e).clone().addClass('shift up'))
 		$(e).before($(e).clone().addClass('shift down'))
 	})
-	//* Face at 60fps
+	// * Call Face every 'interval' ms
 	if (!isMobile())
-		setInterval(() => Face(), 16)
-	$(window).on('scroll', (event) => Face())
+		setInterval(() => Face(), interval)
+	$(window).on('scroll', (event) => {
+		mouse.moved = true
+		Face()
+	})
 	$('.panel-lining.backface').toggle()
 	
-	//* Custom bars
+	// * Custom bars
 	$('bar').replaceWith(function() {
 		let container = $('<div class="bar-container"></div>')
 		let filled = parseInt($(this).attr('value'))
@@ -74,80 +84,81 @@ $(document).ready(function(){
 	})
 
 	//! Listeners
-	//* Perspective mousemovement stuff
-	$(window).on('resize', () => {
-		MouseLeave()
-	})
-	$(document.body).on('mouseleave', MouseLeave)
-	$(document.body).on('mouseenter', () => $allpanels.each((i, e) => $(e).stop()))
-	$(document.body).on('mousemove', (event) => {
-		if (event.screenX == mouse.x && event.screenY == mouse.y)
-			mouse.moved = false
-		else{
+	// * Perspective mousemovement stuff
+	if (!isMobile()) {
+		$(window).on('resize', MouseLeave)
+		$(document.body).on('mouseleave', MouseLeave)
+		$(document.body).on('mouseenter', () => $panels.forEach((e) => $(e[0]).stop()))
+		$(document.body).on('mousemove', (event) => {
 			mouse.x = event.screenX
 			mouse.y = event.screenY
 			mouse.moved = true
-		}
-	})
+		})
+	}
 	//* Page switches
 	$('[data-link]').on('click', function(){SwitchPage($(this).data('link'))})
 	//* Flippables
 	$('.panel.flip').on('click', function(){FlipPanel(this)})
-	
+
 	MouseLeave()
-	
-	function Face(){
-		// face_calls++
-		if (!mouse.moved) return
-		else $mainpanels.each((i, e) => {
+
+	function Face(force = false){
+		face_calls++
+		if (!force) {
+			if (!mouse.moved)
+				return
+			if (!allow_face_call)
+				return
+		}
+		$panels_start.forEach((e, i) => {
 			let rects = e.getBoundingClientRect()
 			let offx = mouse.x - (rects.left + (rects.width / 2))
 			let offy = mouse.y - (rects.top + (rects.height / 2))
 			let angleh = Math.atan(offx / PERSPECTIVE)
 			let anglev = Math.atan(offy / PERSPECTIVE)
-			// $(e).css('--off-x', `${angleh}`)
-			// $(e).css('--off-xa', `${Math.abs(angleh)}`)
-			// $(e).css('--off-y', `${-anglev}`)
 			offsets[i] = {offx: angleh, offxa: Math.abs(angleh), offy: -anglev}
 		})
-		$windows.each((wi, we) => {
-			$(we).children().each((ci, ce) => {
-				$(ce).css('--off-x', `${offsets[ci].offx}`)
-				$(ce).css('--off-xa', `${offsets[ci].offxa}`)
-				$(ce).css('--off-y', `${offsets[ci].offy}`)
-			})
+		$panels.forEach((e) => {
+			if (e[2] == $current_window_id){
+				e[0].style.setProperty('--off-x', `${offsets[e[1]].offx}`)
+				e[0].style.setProperty('--off-xa', `${offsets[e[1]].offxa}`)
+				e[0].style.setProperty('--off-y', `${offsets[e[1]].offy}`)
+			}
 		})
+		mouse.moved = false
+		allow_face_call = false
 	}
 	function MouseLeave(){
 		mouse.moved = false
-		$allpanels.each((i, e) => {
-			let rects = e.getBoundingClientRect()
+		allow_face_call = false
+		$panels.forEach((e) => {
+			let rects = e[0].getBoundingClientRect()
 			mouse.x = window.innerWidth/2
 			mouse.y = window.innerHeight/2
 			let offx = (window.innerWidth/2) - (rects.left + (rects.width / 2))
 			let offy = (window.innerHeight/2) - (rects.top + (rects.height / 2))
-			let angleh = parseFloat($(e).css('--off-x'))
-			let anglev = parseFloat($(e).css('--off-y'))
-			$(e).css('left', angleh)
-			$(e).css('top', anglev)
+			let angleh = parseFloat(e[0].style.getPropertyValue('--off-x'))
+			let anglev = parseFloat(e[0].style.getPropertyValue('--off-y'))
+			$(e[0]).css('left', angleh)
+			$(e[0]).css('top', anglev)
 			angleh = Math.atan(offx / PERSPECTIVE)
 			anglev = Math.atan(offy / PERSPECTIVE)
-			$(e).animate({left: angleh}, {
+			$(e[0]).animate({left: angleh}, {
 				duration: 1000,
 				queue: 'face',
 				step: (now, fx) => {
-					$(e).css('--off-x', `${now}`)
-					$(e).css('--off-xa', `${Math.abs(now)}`)
+					$(e[0]).css('--off-x', `${now}`)
+					$(e[0]).css('--off-xa', `${Math.abs(now)}`)
 				},
-				complete: () => $(e).css('left', '')
+				complete: () => $(e[0]).css('left', '')
 			}).dequeue('face')
-			$(e).animate({top: -anglev}, {
+			$(e[0]).animate({top: -anglev}, {
 				duration: 1000,
 				queue: 'face',
 				step: (now, fx) => {
-					$(e).css('--off-y', `${now}`)
+					$(e[0]).css('--off-y', `${now}`)
 				},
-				complete: () => $(e).css('top', '')
+				complete: () => $(e[0]).css('top', '')
 			}).dequeue('face')
 		})
 	}
@@ -156,14 +167,14 @@ $(document).ready(function(){
 			$('.current').children().find('.flip[style~="--flip:1;"]').click()
 			$('.current').toggleClass('current')
 			$(`#window-${id}`).toggleClass('current')
+			$current_window_id = `window-${id}`
 		}
+		Face(force = true)
 	}
 	function FlipPanel(element){
-		// let deg = ($(element).css('--flip') == '1')? 180 : 0
 		let deg = parseInt(Array.from($(element).css('--rot-y').match(/[0-9]+/g)).pop())
 		if (deg == 1)
 			deg = 0
-		// let final = ($(element).css('--flip') == '1')? 360 : 180
 		let final = deg + 180
 		let toggled = false
 		$(element).toggleClass('flipped')
@@ -177,10 +188,20 @@ $(document).ready(function(){
 				$(element).css('--rot-y', `calc(var(--off-x) * 1rad + ${now}deg)`)
 			}
 		})
-		if (final == 360){
+		if (final == 360)
 			$(element).css('--flip', '0')
-		}else{
+		else
 			$(element).css('--flip', '1')
-		}
+	}
+	function ResetIntervals() {
+		clearInterval(interval_fps_setter)
+		clearInterval(interval_allowcall)
+		// interval_fps_setter = setInterval(() => {
+		// 	interval = parseInt((1000/face_calls) * 0.8)
+		// 	ResetIntervals()
+		// 	face_calls = 0
+		// }, 1000)
+		interval_allowcall = setInterval(() =>
+			allow_face_call = true, interval)
 	}
 })
